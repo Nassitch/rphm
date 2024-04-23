47bfd8f8d2e92631c0f5d4d6a17c0533f4256dd1
Nassime HARMACH <Nassitch@hotmail.fr>
2024-04-23 20:55:21 +0200

    First commit.

---
+++ README.md
@@ -0,0 +1,66 @@
+### Note.
+
+- This library is only available for Angular applications.
+- In the near future, React applications will also be supported.
+
+# Installation & Usage
+
+<h3>
+1. Installation of the package on your Angular project.
+
+```
+npm install rphm_library
+```
+
+2. Script execution (first usage only).
+
+```
+./node_modules/rphm_library/script.sh
+```
+
+3. Script execution (from the second usage).
+
+```
+> rphm
+```
+
+# Operation.
+
+1. The installation of the package from the root of your Angular project via **Node Package Manager** is mandatory for using the library.
+
+2. Upon the first usage of the library on your computer, you must manually navigate to the package to execute the script file from your terminal.
+
+3. After the initial launch of the **script.sh** on your project, a function runs adding an environment variable on your computer allowing to execute this script file on any Angular project with the command **rphm**, provided that the package is installed.
+
+# Details.
+
+<h4>
+
+> 1. Package installation.
+
+- _+1 file_ package.json
+- _+1 file_ README.md
+- _+1 file_ script.sh
+
+> 2.  Execution of the script from the relative path.
+
+- Creation of a _folder_ on the desktop.
+- Cloning of the library _folder_.
+- Transfer of the _file_ **main.css** to the project _folder_.
+- Creation of a _file_ **script.js**.
+- Execution of the **script.js**.
+- Addition of the absolute path of the _file_ **main.css** in **angular.json**.
+- Addition of an environment variable to execute the _file_ script.sh with the command **rphm**.
+- Permanent deletion of the _file_ **script.js**.
+- Permanent deletion of the _file_ **script.sh**.
+
+> 3. Execution of the script using the command _rphm_.
+
+- Creation of a _folder_ on the desktop.
+- Cloning of the library _folder_.
+- Transfer of the _file_ **main.css** to the project _folder_.
+- Creation of a _file_ **script.js**.
+- Execution of the **script.js**.
+- Addition of the absolute path of the _file_ **main.css** in **angular.json**.
+- Permanent deletion of the _file_ **script.js**.
+- Permanent deletion of the _file_ **script.sh**.
---
+++ script.sh
@@ -0,0 +1,164 @@
+#!/bin/bash
+chmod +x "script.sh"
+
+# Définition des variables
+DESKTOP_PATH="$HOME/Desktop"
+FOLDER_PATH="$HOME/Desktop/rphm_library/"
+GIT_CLONE_LIBRARY="git@github.com:raph-bard/css-library.git"
+FOLDER_LIBRARY_PATH="$HOME/Desktop/rphm_library/css-library"
+FILE_STYLE="$FOLDER_LIBRARY_PATH/main.css"
+CURRENT_ANGULAR_PROJECT=$(pwd)
+LIBRARY_IN_ANGULAR_PROJECT="$CURRENT_ANGULAR_PROJECT/src/main.css"
+FILE_ANGULAR_JSON="$CURRENT_ANGULAR_PROJECT/angular.json"
+SCRIPT_JS="script.js"
+SCRIPT_SH="script.sh"
+
+
+function check_folder_exist {
+    if [ -d "$FOLDER_PATH" ]; then
+        echo "🆗 Le dossier 'rphm_library' est existant"
+    else
+        cd "$DESKTOP_PATH"
+        mkdir -p "rphm_library"
+        echo "🆗 Le dossier 'rphm_library' a bien été créé"
+    fi
+}
+
+function clone_library {
+    if [ -d "$FOLDER_LIBRARY_PATH" ]; then
+        echo "🆗 Le dossier 'library' est existant"
+    else
+        git clone "$GIT_CLONE_LIBRARY" "$FOLDER_LIBRARY_PATH"
+        echo "🆗 Le dossier 'library' a bien été créé"
+    fi
+}
+
+function move_file_on_angular {
+    if [ -f "$LIBRARY_IN_ANGULAR_PROJECT" ]; then
+        echo "🆗 La librairie existe déjà dans le projet Angular"
+    else
+        cp "$FILE_STYLE" "$LIBRARY_IN_ANGULAR_PROJECT"
+        echo "🆗 La librairie a bien été ajoutée au projet Angular"
+    fi
+}
+
+function check_angular_json {
+    if [ ! -f "$FILE_ANGULAR_JSON" ]; then
+        echo "❌ Le fichier angular.json n'existe pas."
+        return 1
+    fi
+
+}
+
+# function add_dependence_on_angular_json {
+
+# cd $CURRENT_ANGULAR_PROJECT
+# }
+
+# Creation de script.js
+function creating_of_js_script {
+cd $CURRENT_ANGULAR_PROJECT
+touch script.js
+
+cat <<EOF > script.js
+const fs = require("fs");
+
+/* Variable de dossier et fichier. */
+let projectName;
+const angularJsonPath = "angular.json";
+const libraryPath = "src/main.css";
+/* Variable formatage. */
+let angularJsonContent = fs.readFileSync(angularJsonPath, "utf8");
+let angularJson = JSON.parse(angularJsonContent);
+
+function getProjectName() {
+  try {
+    const projectKeys = Object.keys(angularJson.projects);
+    projectName = projectKeys[0];
+    return projectName;
+  } catch (error) {
+    console.error(
+      "Une erreur s'est produite lors de la lecture de angular.json :",
+      error
+    );
+    return projectName;
+  }
+}
+
+if (projectName) {
+  console.log(projectName);
+}
+
+function insertLibraryPath() {
+  try {
+    if (!projectName) {
+      console.log(
+        "❌ Le nom du projet n'a pas été trouvé dans angular.json..........."
+      );
+      return;
+    } else {
+      console.log(\`🆗 Le projet \${projectName} est existant.\`);
+    }
+
+    const pathToInjectDependences =
+      angularJson.projects[projectName].architect.build.options.styles;
+
+    if (pathToInjectDependences[0] !== libraryPath) {
+      pathToInjectDependences.unshift(libraryPath);
+      fs.writeFileSync(angularJsonPath, JSON.stringify(angularJson, null, 2));
+      console.log(\`🆗 La librairie à bien été ajouté aux dépendences.\`);
+    } else {
+      console.log(\`🆗 La librairie se trouve déjà dans les dépendences.\`);
+    }
+  } catch (error) {
+    console.log(
+      "❌ Une erreur s'est produite lors de la lecture/écriture de angular.json :",
+      error
+    );
+  }
+}
+
+getProjectName();
+insertLibraryPath();
+EOF
+
+PROJECT_NAME=$(node "$SCRIPT_JS")
+    echo "$PROJECT_NAME"
+}
+
+function add_commande_line_for_execute {
+  if ! grep -q "alias rphm='./node_modules/rphm_library/script.sh'" ~/.bashrc; then
+    echo "alias rphm='./node_modules/rphm_library/script.sh'" >> ~/.bashrc
+    source ~/.bashrc
+    echo "Alias 'rphm' ajouté avec succès."
+  else
+    echo "L'alias 'rphm' existe déjà dans ~/.bashrc."
+  fi
+
+  if ! grep -q "alias rphm='./node_modules/rphm_library/script.sh'" ~/.zshrc; then
+    echo "alias rphm='./node_modules/rphm_library/script.sh'" >> ~/.zshrc
+    source ~/.zshrc
+    echo "Alias 'rphm' ajouté avec succès."
+  else
+    echo "L'alias 'rphm' existe déjà dans ~/.zshrc."
+  fi
+}
+
+# Destruction de dossiers et fichiers.
+function self_destruction {
+    echo "✔️ La librairie est correctement installée."
+    rm -rf "$CURRENT_ANGULAR_PROJECT/script.js"
+    rm -rf $FOLDER_PATH
+    echo "🔥 Auto-destruction iminente."
+    # rm -rf "$CURRENT_ANGULAR_PROJECT/script.sh"
+}
+
+# Appel des fonctions
+creating_of_js_script
+check_folder_exist
+clone_library
+move_file_on_angular
+check_angular_json
+add_commande_line_for_execute
+# add_dependence_on_angular_json
+self_destruction
